20cb98530adb0dcca5fa03dedb383a0e91c7ef2b
mnalishuwa <mnalishuwa@gmail.com>
2023-11-03 10:06:38 +0000

    update 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,8 +17,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	if (binary_tree_is_full(tree) && (binary_tree_balance(tree) == 0 &&
-					  binary_tree_balance(tree) == 0 &&
-					  binary_tree_balance(tree) == 0))
+					  binary_tree_balance(tree->left) == 0 &&
+					  binary_tree_balance(tree->right) == 0))
 		return (1);
 
 	return (0);
